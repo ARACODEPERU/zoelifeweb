e0a1b97408b0e2488fd0844e2277442e9437875c
Yisus Web <connexion.jesus@gmail.com>
2023-11-01 14:06:21 -0500

    banderas

--- resources/views/zoelife/index.blade.php
+++ resources/views/zoelife/index.blade.php
@@ -15,6 +15,7 @@
                             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                                 <div id="gen-menu-contain" class="gen-menu-contain">
                                     <ul id="gen-main-menu" class="navbar-nav ml-auto">
+                                        <!--
                                         <li class="menu-item active">
                                             <a href="#" aria-current="page">Home</a>
                                             <i class="fa fa-chevron-down gen-submenu-icon"></i>
@@ -33,166 +34,36 @@
                                                 </li>
                                             </ul>
                                         </li>
-                                        <li class="menu-item">
-                                            <a href="#">Movies</a>
-                                            <i class="fa fa-chevron-down gen-submenu-icon"></i>
-                                            <ul class="sub-menu">
-                                                <li class="menu-item menu-item-has-children">
-                                                    <a href="#">Movies List</a>
-                                                    <i class="fa fa-chevron-down gen-submenu-icon"></i>
-                                                    <ul class="sub-menu">
-                                                        <li class="menu-item">
-                                                            <a href="movies-load-more.html">Load More</a>
-                                                        </li>
-                                                        <li class="menu-item">
-                                                            <a href="movies-infinite-scroll.html">Infinite scroll</a>
-                                                        </li>
-                                                        <li class="menu-item">
-                                                            <a href="movies-pagination.html">Pagination</a>
-                                                        </li>
-                                                    </ul>
-                                                </li>
-                                                <li class="menu-item menu-item-has-children">
-                                                    <a href="#">Movies Style</a>
-                                                    <i class="fa fa-chevron-down gen-submenu-icon"></i>
-                                                    <ul class="sub-menu">
-                                                        <li class="menu-item">
-                                                            <a href="movies-style-1.html">Style 1</a>
-                                                        </li>
-                                                        <li class="menu-item">
-                                                            <a href="movies-style-2.html">Style 2</a>
-                                                        </li>
-                                                        <li class="menu-item">
-                                                            <a href="movies-style-3.html">Style 3</a>
-                                                        </li>
-                                                    </ul>
-                                                </li>
-                                                <li class="menu-item">
-                                                    <a href="single-movie.html">Single Movie</a>
-                                                </li>
-                                            </ul>
+                                        -->
+                                        <li>
+                                            <a href="#" aria-current="page">
+                                                <img style="width: 25px;" src="{{ asset('themes/zoelife/global/images/flags/peru.png') }}" alt="">
+                                                &nbsp;Perú
+                                            </a>
                                         </li>
-                                        <li class="menu-item">
-                                            <a href="#">Tv Shows</a>
-                                            <i class="fa fa-chevron-down gen-submenu-icon"></i>
-                                            <ul class="sub-menu">
-                                                <li class="menu-item menu-item-has-children">
-                                                    <a href="#">Tv Shows List</a>
-                                                    <i class="fa fa-chevron-down gen-submenu-icon"></i>
-                                                    <ul class="sub-menu">
-                                                        <li class="menu-item">
-                                                            <a href="tv-shows-load-more.html">Load More</a>
-                                                        </li>
-                                                        <li class="menu-item">
-                                                            <a href="tv-shows-infinite-scroll.html">Infinite scroll</a>
-                                                        </li>
-                                                        <li class="menu-item">
-                                                            <a href="tv-shows-pagination.html">Pagination</a>
-                                                        </li>
-                                                    </ul>
-                                                </li>
-                                                <li class="menu-item menu-item-has-children">
-                                                    <a href="#">Tv Shows Style</a>
-                                                    <i class="fa fa-chevron-down gen-submenu-icon"></i>
-                                                    <ul class="sub-menu">
-                                                        <li class="menu-item">
-                                                            <a href="tv-shows-style-1.html">Style 1</a>
-                                                        </li>
-                                                        <li class="menu-item">
-                                                            <a href="tv-shows-style-2.html">Style 2</a>
-                                                        </li>
-                                                        <li class="menu-item">
-                                                            <a href="tv-shows-style-3.html">Style 3</a>
-                                                        </li>
-                                                    </ul>
-                                                </li>
-                                                <li class="menu-item">
-                                                    <a href="single-tv-shows.html">Single Tv Shows</a>
-                                                </li>
-                                                <li class="menu-item">
-                                                    <a href="single-episode.html">Single Episode</a>
-                                                </li>
-                                            </ul>
+                                        <li>
+                                            <a href="#" aria-current="page">
+                                                <img style="width: 25px;" src="{{ asset('themes/zoelife/global/images/flags/bolivia.png') }}" alt="">
+                                                &nbsp;Bolivia
+                                            </a>
                                         </li>
-                                        <li class="menu-item">
-                                            <a href="#">Video</a>
-                                            <i class="fa fa-chevron-down gen-submenu-icon"></i>
-                                            <ul class="sub-menu">
-                                                <li class="menu-item menu-item-has-children">
-                                                    <a href="#">Video</a>
-                                                    <i class="fa fa-chevron-down gen-submenu-icon"></i>
-                                                    <ul class="sub-menu">
-                                                        <li class="menu-item">
-                                                            <a href="video-load-more.html">Load More</a>
-                                                        </li>
-                                                        <li class="menu-item">
-                                                            <a href="video-infinite-scroll.html">Infinite scroll</a>
-                                                        </li>
-                                                        <li class="menu-item">
-                                                            <a href="video-pagination.html">Pagination</a>
-                                                        </li>
-                                                    </ul>
-                                                </li>
-                                                <li class="menu-item menu-item-has-children">
-                                                    <a href="#">Videos Style</a>
-                                                    <i class="fa fa-chevron-down gen-submenu-icon"></i>
-                                                    <ul class="sub-menu">
-                                                        <li class="menu-item">
-                                                            <a href="videos-style-1.html">Style 1</a>
-                                                        </li>
-                                                        <li class="menu-item">
-                                                            <a href="videos-style-2.html">Style 2</a>
-                                                        </li>
-                                                        <li class="menu-item">
-                                                            <a href="videos-style-3.html">Style 3</a>
-                                                        </li>
-                                                    </ul>
-                                                </li>
-                                                <li class="menu-item">
-                                                    <a href="single-videos.html">Single videos</a>
-                                                </li>
-                                            </ul>
+                                        <li>
+                                            <a href="#" aria-current="page">
+                                                <img style="width: 25px;" src="{{ asset('themes/zoelife/global/images/flags/ecuador.png') }}" alt="">
+                                                &nbsp;Ecuador
+                                            </a>
                                         </li>
-                                        <li class="menu-item">
-                                            <a href="#">Pages</a>
-                                            <i class="fa fa-chevron-down gen-submenu-icon"></i>
-                                            <ul class="sub-menu">
-                                                <li class="menu-item menu-item-has-children">
-                                                    <a href="#">Blog</a>
-                                                    <i class="fa fa-chevron-down gen-submenu-icon"></i>
-                                                    <ul class="sub-menu">
-                                                        <li class="menu-item menu-item-has-children">
-                                                            <a href="#">Blog With Sidebar</a>
-                                                            <i class="fa fa-chevron-down gen-submenu-icon"></i>
-                                                            <ul class="sub-menu">
-                                                                <li class="menu-item">
-                                                                    <a href="blog-left-sidebar.html">blog left
-                                                                        sidebar</a>
-                                                                </li>
-                                                                <li class="menu-item">
-                                                                    <a href="blog-right-sidebar.html">blog right
-                                                                        sidebar</a>
-                                                                </li>
-                                                            </ul>
-                                                        </li>
-                                                    </ul>
-                                                </li>
-                                                <li class="menu-item menu-item-has-children">
-                                                    <a href="#">Pricing</a>
-                                                    <i class="fa fa-chevron-down gen-submenu-icon"></i>
-                                                    <ul class="sub-menu">
-                                                        <li class="menu-item">
-                                                            <a href="pricing-style-1.html">Style 1</a>
-                                                        </li>
-                                                        <li class="menu-item">
-                                                            <a href="pricing-style-2.html">Style 2</a>
-                                                        </li>
-                                                    </ul>
-                                                </li>
-                                                <li class="menu-item">
-                                                    <a href="contact-us.html">Contact Us</a>
-                                                </li>
-                                            </ul>
+                                        <li>
+                                            <a href="#" aria-current="page">
+                                                <img style="width: 25px;" src="{{ asset('themes/zoelife/global/images/flags/colombia.png') }}" alt="">
+                                                &nbsp;Colombia
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#" aria-current="page">
+                                                <img style="width: 25px;" src="{{ asset('themes/zoelife/global/images/flags/mexico.png') }}" alt="">
+                                                &nbsp;Mexico
+                                            </a>
                                         </li>
                                     </ul>
                                 </div>
@@ -254,7 +125,7 @@
                                     <a href="register.html" class="gen-button">
                                         <div class="gen-button-block">
                                             <span class="gen-button-line-left"></span>
-                                            <span class="gen-button-text">Subscribe</span>
+                                            <span class="gen-button-text">Oficina Virutal</span>
                                         </div>
                                     </a>
                                 </div>
