c6644ada7c573a131bda84a8c74f625e431a277f
Yisus Web <connexion.jesus@gmail.com>
2023-11-08 15:08:40 -0500

    Primeros componentes

---
+++ resources/views/layouts/peru.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+	<meta name="robots" content="" />
+	<meta name="description" content="CargoZone - Transport and Cargo Template" />
+	<meta property="og:title" content="CargoZone - Transport and Cargo Template" />
+	<meta property="og:description" content="CargoZone - Transport and Cargo Template" />
+	<meta property="og:image" content=""/>
+	<meta name="format-detection" content="telephone=no">
+	
+	<!-- FAVICONS ICON -->
+	<link rel="icon" href="themes/zoelife/paises/images/favicon.ico" type="image/x-icon" />
+	<link rel="shortcut icon" type="image/x-icon" href="themes/zoelife/paises/images/favicon.png" />
+	
+	<!-- PAGE TITLE HERE -->
+	<title>CargoZone - Transport and Cargo Template </title>
+	
+	<!-- MOBILE SPECIFIC -->
+	<meta name="viewport" content="width=device-width">
+	
+	<!--[if lt IE 9]>
+	<script src="js/html5shiv.min.js"></script>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+	
+	<!-- STYLESHEETS -->
+	<link rel="stylesheet" type="text/css" href="themes/zoelife/paises/css/plugins.css">
+	<link rel="stylesheet" type="text/css" href="themes/zoelife/paises/css/style.css">
+	<link rel="stylesheet" type="text/css" href="themes/zoelife/paises/css/templete.css">
+	<link rel="stylesheet" type="text/css" class="skin" href="themes/zoelife/paises/css/skin/skin-9.css">
+	
+	<!-- REVOLUTION SLIDER CSS -->
+	<link rel="stylesheet" type="text/css" href="themes/zoelife/paises/plugins/revolution/revolution/css/settings.css">
+	<link rel="stylesheet" type="text/css" href="themes/zoelife/paises/plugins/revolution/revolution/css/navigation.css">
+
+</head>
+<body id="bg">
+<div class="page-wraper">
+
+    @yield('content')
+    <!-- Back-to-Top end -->
+
+
+
+    <button class="scroltop fa fa-arrow-up" ></button>
+</div>
+<!-- JavaScript files ========================================= -->
+<script src="themes/zoelife/paises/js/jquery.min.js"></script><!-- JQUERY.MIN JS -->
+<script src="themes/zoelife/paises/plugins/bootstrap/js/popper.min.js"></script><!-- BOOTSTRAP.MIN JS -->
+<script src="themes/zoelife/paises/plugins/bootstrap/js/bootstrap.min.js"></script><!-- BOOTSTRAP.MIN JS -->
+<script src="themes/zoelife/paises/plugins/bootstrap-select/bootstrap-select.min.js"></script><!-- FORM JS -->
+<script src="themes/zoelife/paises/plugins/bootstrap-touchspin/jquery.bootstrap-touchspin.js"></script><!-- FORM JS -->
+<script src="themes/zoelife/paises/plugins/magnific-popup/magnific-popup.js"></script><!-- MAGNIFIC POPUP JS -->
+<script src="themes/zoelife/paises/plugins/counter/waypoints-min.js"></script><!-- WAYPOINTS JS -->
+<script src="themes/zoelife/paises/plugins/counter/counterup.min.js"></script><!-- COUNTERUP JS -->
+<script src="themes/zoelife/paises/plugins/imagesloaded/imagesloaded.js"></script><!-- IMAGESLOADED -->
+<script src="themes/zoelife/paises/plugins/masonry/masonry-3.1.4.js"></script><!-- MASONRY -->
+<script src="themes/zoelife/paises/plugins/masonry/masonry.filter.js"></script><!-- MASONRY -->
+<script src="themes/zoelife/paises/plugins/owl-carousel/owl.carousel.js"></script><!-- OWL SLIDER -->
+<script src="themes/zoelife/paises/plugins/switcher/js/switcher.js"></script><!-- SWITCHER JS  -->
+<script src="themes/zoelife/paises/js/custom.js"></script><!-- CUSTOM FUCTIONS  -->
+<script src="themes/zoelife/paises/js/dz.carousel.js"></script><!-- SORTCODE FUCTIONS  -->
+<script src="themes/zoelife/paises/js/dz.ajax.js"></script><!-- CONTACT JS  -->
+<!-- REVOLUTION JS FILES -->
+<script src="themes/zoelife/paises/plugins/revolution/revolution/js/jquery.themepunch.tools.min.js"></script>
+<script src="themes/zoelife/paises/plugins/revolution/revolution/js/jquery.themepunch.revolution.min.js"></script>
+<!-- Slider revolution 5.0 Extensions  (Load Extensions only on Local File Systems !  The following part can be removed on Server for On Demand Loading) -->
+<script src="themes/zoelife/paises/plugins/revolution/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+<script src="themes/zoelife/paises/plugins/revolution/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+<script src="themes/zoelife/paises/plugins/revolution/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script src="themes/zoelife/paises/plugins/revolution/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script src="themes/zoelife/paises/plugins/revolution/revolution/js/extensions/revolution.extension.migration.min.js"></script>
+<script src="themes/zoelife/paises/plugins/revolution/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+<script src="themes/zoelife/paises/plugins/revolution/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+<script src="themes/zoelife/paises/plugins/revolution/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script src="themes/zoelife/paises/plugins/revolution/revolution/js/extensions/revolution.extension.video.min.js"></script>
+<script src="themes/zoelife/paises/js/rev.slider.js"></script>
+<script>
+jQuery(document).ready(function() {
+	'use strict';
+	dz_rev_slider_4();
+});	/*ready*/
+</script>
+<div class="loading-area" style="display:none;">
+    <div class="loading-box"></div>
+    <div class="loading-pic"></div>
+</div>
+</body>
+</html>
