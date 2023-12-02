8259eb011ceedbba85306c90d2f57f3cce15bef1
Yisus Web <connexion.jesus@gmail.com>
2023-12-02 11:21:05 -0500

    ploikju

--- resources/views/layouts/peru.blade.php
+++ resources/views/layouts/peru.blade.php
@@ -13,8 +13,8 @@
 	<meta name="format-detection" content="telephone=no">
 	
 	<!-- FAVICONS ICON -->
-	<link rel="icon" href="themes/zoelife/global/images/favicon.ico" type="image/x-icon" />
-	<link rel="shortcut icon" type="image/x-icon" href="themes/zoelife/global/images/favicon.png" />
+	<link rel="icon" href="{{ asset('themes/zoelife/global/images/favicon.ico') }}" type="image/x-icon" />
+	<link rel="shortcut icon" type="image/x-icon" href="{{ asset('themes/zoelife/global/images/favicon.png') }}" />
 	
 	<!-- PAGE TITLE HERE -->
 	<title>Perú | ZoeLife International </title>
@@ -28,15 +28,15 @@
 	<![endif]-->
 	
 	<!-- STYLESHEETS -->
-	<link rel="stylesheet" type="text/css" href="themes/zoelife/peru/css/plugins.css">
-	<link rel="stylesheet" type="text/css" href="themes/zoelife/peru/css/style.css">
-	<link rel="stylesheet" type="text/css" href="themes/zoelife/peru/css/templete.css">
-	<link rel="stylesheet" type="text/css" class="skin" href="themes/zoelife/peru/css/skin/skin-9.css">
-	<link rel="stylesheet" type="text/css" class="skin" href="themes/zoelife/peru/css/aracode.css">
+	<link rel="stylesheet" type="text/css" href="{{ asset('themes/zoelife/peru/css/plugins.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('themes/zoelife/peru/css/style.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('themes/zoelife/peru/css/templete.css') }}">
+	<link rel="stylesheet" type="text/css" class="skin" href="{{ asset('themes/zoelife/peru/css/skin/skin-9.css') }}">
+	<link rel="stylesheet" type="text/css" class="skin" href="{{ asset('themes/zoelife/peru/css/aracode.css') }}">
 	
 	<!-- REVOLUTION SLIDER CSS -->
-	<link rel="stylesheet" type="text/css" href="themes/zoelife/peru/plugins/revolution/revolution/css/settings.css">
-	<link rel="stylesheet" type="text/css" href="themes/zoelife/peru/plugins/revolution/revolution/css/navigation.css">
+	<link rel="stylesheet" type="text/css" href="{{ asset('themes/zoelife/peru/plugins/revolution/revolution/css/settings.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('themes/zoelife/peru/plugins/revolution/revolution/css/navigation.css') }}">
 
 	
 
@@ -52,36 +52,36 @@
     <button class="scroltop fa fa-arrow-up" ></button>
 </div>
 <!-- JavaScript files ========================================= -->
-<script src="themes/zoelife/peru/js/jquery.min.js"></script><!-- JQUERY.MIN JS -->
-<script src="themes/zoelife/peru/plugins/bootstrap/js/popper.min.js"></script><!-- BOOTSTRAP.MIN JS -->
-<script src="themes/zoelife/peru/plugins/bootstrap/js/bootstrap.min.js"></script><!-- BOOTSTRAP.MIN JS -->
-<script src="themes/zoelife/peru/plugins/bootstrap-select/bootstrap-select.min.js"></script><!-- FORM JS -->
-<script src="themes/zoelife/peru/plugins/bootstrap-touchspin/jquery.bootstrap-touchspin.js"></script><!-- FORM JS -->
-<script src="themes/zoelife/peru/plugins/magnific-popup/magnific-popup.js"></script><!-- MAGNIFIC POPUP JS -->
-<script src="themes/zoelife/peru/plugins/counter/waypoints-min.js"></script><!-- WAYPOINTS JS -->
-<script src="themes/zoelife/peru/plugins/counter/counterup.min.js"></script><!-- COUNTERUP JS -->
-<script src="themes/zoelife/peru/plugins/imagesloaded/imagesloaded.js"></script><!-- IMAGESLOADED -->
-<script src="themes/zoelife/peru/plugins/masonry/masonry-3.1.4.js"></script><!-- MASONRY -->
-<script src="themes/zoelife/peru/plugins/masonry/masonry.filter.js"></script><!-- MASONRY -->
-<script src="themes/zoelife/peru/plugins/owl-carousel/owl.carousel.js"></script><!-- OWL SLIDER -->
-<script src="themes/zoelife/peru/plugins/switcher/js/switcher.js"></script><!-- SWITCHER JS  -->
-<script src="themes/zoelife/peru/js/custom.js"></script><!-- CUSTOM FUCTIONS  -->
-<script src="themes/zoelife/peru/js/dz.carousel.js"></script><!-- SORTCODE FUCTIONS  -->
-<script src="themes/zoelife/peru/js/dz.ajax.js"></script><!-- CONTACT JS  -->
+<script src="{{ asset('themes/zoelife/peru/js/jquery.min.js') }}"></script><!-- JQUERY.MIN JS -->
+<script src="{{ asset('themes/zoelife/peru/plugins/bootstrap/js/popper.min.js') }}"></script><!-- BOOTSTRAP.MIN JS -->
+<script src="{{ asset('themes/zoelife/peru/plugins/bootstrap/js/bootstrap.min.js') }}"></script><!-- BOOTSTRAP.MIN JS -->
+<script src="{{ asset('themes/zoelife/peru/plugins/bootstrap-select/bootstrap-select.min.js') }}"></script><!-- FORM JS -->
+<script src="{{ asset('themes/zoelife/peru/plugins/bootstrap-touchspin/jquery.bootstrap-touchspin.js') }}"></script><!-- FORM JS -->
+<script src="{{ asset('themes/zoelife/peru/plugins/magnific-popup/magnific-popup.js') }}"></script><!-- MAGNIFIC POPUP JS -->
+<script src="{{ asset('themes/zoelife/peru/plugins/counter/waypoints-min.js') }}"></script><!-- WAYPOINTS JS -->
+<script src="{{ asset('themes/zoelife/peru/plugins/counter/counterup.min.js') }}"></script><!-- COUNTERUP JS -->
+<script src="{{ asset('themes/zoelife/peru/plugins/imagesloaded/imagesloaded.js') }}"></script><!-- IMAGESLOADED -->
+<script src="{{ asset('themes/zoelife/peru/plugins/masonry/masonry-3.1.4.js') }}"></script><!-- MASONRY -->
+<script src="{{ asset('themes/zoelife/peru/plugins/masonry/masonry.filter.js') }}"></script><!-- MASONRY -->
+<script src="{{ asset('themes/zoelife/peru/plugins/owl-carousel/owl.carousel.js') }}"></script><!-- OWL SLIDER -->
+<script src="{{ asset('themes/zoelife/peru/plugins/switcher/js/switcher.js') }}"></script><!-- SWITCHER JS  -->
+<script src="{{ asset('themes/zoelife/peru/js/custom.js') }}"></script><!-- CUSTOM FUCTIONS  -->
+<script src="{{ asset('themes/zoelife/peru/js/dz.carousel.js') }}"></script><!-- SORTCODE FUCTIONS  -->
+<script src="{{ asset('themes/zoelife/peru/js/dz.ajax.js') }}"></script><!-- CONTACT JS  -->
 <!-- REVOLUTION JS FILES -->
-<script src="themes/zoelife/peru/plugins/revolution/revolution/js/jquery.themepunch.tools.min.js"></script>
-<script src="themes/zoelife/peru/plugins/revolution/revolution/js/jquery.themepunch.revolution.min.js"></script>
+<script src="{{ asset('themes/zoelife/peru/plugins/revolution/revolution/js/jquery.themepunch.tools.min.js') }}"></script>
+<script src="{{ asset('themes/zoelife/peru/plugins/revolution/revolution/js/jquery.themepunch.revolution.min.js') }}"></script>
 <!-- Slider revolution 5.0 Extensions  (Load Extensions only on Local File Systems !  The following part can be removed on Server for On Demand Loading) -->
-<script src="themes/zoelife/peru/plugins/revolution/revolution/js/extensions/revolution.extension.actions.min.js"></script>
-<script src="themes/zoelife/peru/plugins/revolution/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
-<script src="themes/zoelife/peru/plugins/revolution/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
-<script src="themes/zoelife/peru/plugins/revolution/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
-<script src="themes/zoelife/peru/plugins/revolution/revolution/js/extensions/revolution.extension.migration.min.js"></script>
-<script src="themes/zoelife/peru/plugins/revolution/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
-<script src="themes/zoelife/peru/plugins/revolution/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
-<script src="themes/zoelife/peru/plugins/revolution/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
-<script src="themes/zoelife/peru/plugins/revolution/revolution/js/extensions/revolution.extension.video.min.js"></script>
-<script src="themes/zoelife/peru/js/rev.slider.js"></script>
+<script src="{{ asset('themes/zoelife/peru/plugins/revolution/revolution/js/extensions/revolution.extension.actions.min.js') }}"></script>
+<script src="{{ asset('themes/zoelife/peru/plugins/revolution/revolution/js/extensions/revolution.extension.carousel.min.js') }}"></script>
+<script src="{{ asset('themes/zoelife/peru/plugins/revolution/revolution/js/extensions/revolution.extension.kenburn.min.js') }}"></script>
+<script src="{{ asset('themes/zoelife/peru/plugins/revolution/revolution/js/extensions/revolution.extension.layeranimation.min.js') }}"></script>
+<script src="{{ asset('themes/zoelife/peru/plugins/revolution/revolution/js/extensions/revolution.extension.migration.min.js') }}"></script>
+<script src="{{ asset('themes/zoelife/peru/plugins/revolution/revolution/js/extensions/revolution.extension.navigation.min.js') }}"></script>
+<script src="{{ asset('themes/zoelife/peru/plugins/revolution/revolution/js/extensions/revolution.extension.parallax.min.js') }}"></script>
+<script src="{{ asset('themes/zoelife/peru/plugins/revolution/revolution/js/extensions/revolution.extension.slideanims.min.js') }}"></script>
+<script src="{{ asset('themes/zoelife/peru/plugins/revolution/revolution/js/extensions/revolution.extension.video.min.js') }}"></script>
+<script src="{{ asset('themes/zoelife/peru/js/rev.slider.js') }}"></script>
 <script>
 jQuery(document).ready(function() {
 	'use strict';
