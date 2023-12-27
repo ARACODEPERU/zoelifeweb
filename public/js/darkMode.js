c1afb63cf344f2f303cca221a15f1175660b4e8f
Grandejosh <60234854+Grandejosh@users.noreply.github.com>
2023-12-26 23:12:21 -0500

    correcion darkMode en el JS

--- public/js/darkMode.js
+++ public/js/darkMode.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
       a_tags.forEach((a_tag) => {
         a_tag.classList.toggle("dark");
       });
-      const bg_whites = document.querySelectorAll(".selector-d");
+      let bg_whites = document.querySelectorAll(".selector-d");
       bg_whites.forEach((bg_white) => {
         bg_white.classList.toggle("bg-white");
       });
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
       a_tags.forEach((a_tag) => {
         a_tag.classList.toggle("dark");
       });
-      const bg_whites = document.querySelectorAll(".selector-d");
+      let bg_whites = document.querySelectorAll(".selector-d");
       bg_whites.forEach((bg_white) => {
         bg_white.classList.toggle("bg-white");
       });
