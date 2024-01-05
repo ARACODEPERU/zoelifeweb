c2571e2a1a963b8a25702deb4b8d661ca13a5ae7
Yisus Web <connexion.jesus@gmail.com>
2024-01-05 14:29:06 -0500

    tyui

--- resources/views/components/peru/footer-area.blade.php
+++ resources/views/components/peru/footer-area.blade.php
@@ -35,7 +35,7 @@
                                     @endforeach
                                 @endif
                             </ul>
-                            <a href="" class="site-button">Ver más</a>
+                            <a href="" style="color: orange;"> Ver más testimonios</a>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6 col-sm-6">
