70c31e18ced2a51e096367e86c8cb7a0422f478d
Yisus Web <connexion.jesus@gmail.com>
2024-04-03 17:22:12 -0500

    css peru

--- resources/views/zoelife/peru/producto-descripcion.blade.php
+++ resources/views/zoelife/peru/producto-descripcion.blade.php
@@ -29,8 +29,8 @@
                     <div class="p-a30 bg-white selector-d m-b30">
                         <div class="section-content">
                             <h2 class="text-uppercase">{{ $product->name }}</h2>
-                            <div class="dez-divider divider-2px bg-primary icon-left">
-                                <i class="fa fa-thumbs-o-up bg-primary text-white"></i>
+                            <div class="dez-divider divider-2px icon-left" style="background: #00b01a;">
+                                <i class="fa fa-thumbs-o-up  text-white" style="background: #00b01a;"></i>
                             </div>
                             <div class="m-b10" >
                                 {!! $product->description !!}
@@ -49,7 +49,7 @@
                 <div class="row">
                     <div class="col-md-6">
                         <div class="section-head  text-center text-white">
-                            <h2 class="h2">Recomendaciones <span class="text-primary"> de Uso Sugerido</span></h2>
+                            <h2 class="h2">Recomendaciones <span style="color: #00b01a;"> de Uso Sugerido</span></h2>
                             <div class="dez-separator text-primary style-icon border-white">
                                 <img alt="" src="{{ asset('themes/zoelife/peru/images/leaf-green.png') }}" />
                             </div>
@@ -75,8 +75,8 @@
                             <p>
                                 Sumérgete en una experiencia única y conoce de primera mano cómo nuestro producto ha superado las expectativas y ha transformado rutinas diarias en momentos extraordinarios.
                             </p>
-                            <div class="dez-divider divider-2px bg-primary icon-center"><i
-                                    class="fa fa-truck bg-primary text-white"></i></div>
+                            <div class="dez-divider divider-2px  icon-center" style="background: #00b01a;"><i
+                                    class="fa fa-truck  text-white" style="background: #00b01a;"></i></div>
                         </div>
                     </div>
                 </div>
