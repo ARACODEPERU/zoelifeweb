bb68db6df198e58dbfce785b465119e48a2b859f
Yisus Web <connexion.jesus@gmail.com>
2024-04-04 16:15:31 -0500

    koiut

--- resources/views/zoelife/peru/index.blade.php
+++ resources/views/zoelife/peru/index.blade.php
@@ -61,7 +61,7 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-md-12">
-                            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="width: 90%; height: auto;">
+                            <div id="carouselExampleIndicators" class="slide" data-ride="carousel" style="width: 100%; height: auto;">
                                 <ol class="carousel-indicators">
                                     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                                     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
