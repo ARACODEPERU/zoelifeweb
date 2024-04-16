51a1752bfa1d78d85c7527e59a152fb2134349a1
Elmer Rodriguez <elrodriguez2423@gmail.com>
2024-04-16 11:04:28 -0500

    el

--- Modules/Sales/Resources/assets/js/Pages/Products/List.vue
+++ Modules/Sales/Resources/assets/js/Pages/Products/List.vue
@@ -435,10 +435,10 @@
                       </td>
                         <td class="text-center border-b border-stroke py-4 px-4 pl-9 dark:border-strokedark xl:pl-11">
                           <div class="flex items-center space-x-3.5">
-                              <Link v-permission="'productos_editar'" title="Editar" :href="route('products.edit',product.id)" class="mr-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                              <!-- <Link v-permission="'productos_editar'" title="Editar" :href="route('products.edit',product.id)" class="mr-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                   <font-awesome-icon :icon="faPencilAlt" />
-                              </Link>
-                              <template v-if="form.displayProduct" >
+                              </Link> -->
+                              <!-- <template v-if="form.displayProduct" >
                                 <button title="Mover Mercadería/Calzados" type="button" class="mr-1 text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
                                 @click="openModalTrasladoMercaderia(product)"
                                 >
@@ -459,7 +459,7 @@
                                 >
                                   <font-awesome-icon :icon="faWarehouse" />
                                 </button>
-                              </template> 
+                              </template>  -->
                               <button type="button" class="mr-1 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
                                 @click="destroy(product.id)"
                                 >
