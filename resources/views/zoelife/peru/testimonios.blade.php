c4dd8cbf98315e8fd94734eb27bbf57441d4563e
Yisus Web <connexion.jesus@gmail.com>
2024-09-12 13:13:43 -0500

    fer

--- resources/views/zoelife/peru/testimonios.blade.php
+++ resources/views/zoelife/peru/testimonios.blade.php
@@ -35,7 +35,7 @@
             <!-- Breadcrumb row END -->
         </div>
 
-        {{-- <div class="content-area">
+        <div class="content-area">
             <div class="">
                 <div class="site-filters clearfix center m-b40">
                     <ul class="filters" data-toggle="buttons">
@@ -78,54 +78,7 @@
                     @endif
                 </ul>
             </div>
-        </div> --}}
-        <br>
-        <section>
-            <div class="container-xxl">
-                <div class="row">
-                    <div class="site-filters clearfix center m-b40">
-                        <ul class="filters" data-toggle="buttons">
-                            <li data-filter="" class="btn active">
-                                <input type="radio">
-                                <a href="#" class="site-button-secondry"><span>Todos</span></a>
-                            </li>
-                            @if (count($products) > 0)
-                                @foreach ($products as $product)
-                                    <li data-filter="{{ $product->name }}" class="btn">
-                                        <input type="radio">
-                                        <a href="#" class="site-button-secondry"><span>{{ $product->name }}</span></a>
-                                    </li>
-                                @endforeach
-                            @endif
-                        </ul>
-                    </div>
-                    <ul id="masonry" class="dez-gallery-listing row gallery-grid-4 m-b0 mfp-gallery">
-                        @if (count($testimonies) > 0)
-                            @foreach ($testimonies as $testimony)
-                                <li class="card-container col-md-4 {{ $testimony->product->name }}" style="padding: 15px;">
-                                    <div class="aracode-box-with">
-                                        <div class="dez-post-media dez-img-effect zoom-slow">
-                                            {!! $testimony->video !!}
-                                        </div>
-                                        <div class="dez-post-info">
-                                            <div class="dez-post-title ">
-                                                <h4 class="post-title">
-                                                    <a href="" class=" des-producto">{{ $testimony->title }}</a>
-                                                </h4>
-                                            </div>
-                                            <div class="dez-post-tags">
-                                                <div class="post-tags">
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li>
-                            @endforeach
-                        @endif
-                    </ul>
-                </div>
-            </div>
-        </section>
+        </div>
 
     </div>
     <!-- Content END-->
