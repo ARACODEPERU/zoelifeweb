750e3c1d94f315707b7fdf6b2e552b401fac85dd
Yisus Web <connexion.jesus@gmail.com>
2024-10-15 11:46:27 -0500

    oikjm

--- resources/views/zoelife/mexico/producto-descripcion.blade.php
+++ resources/views/zoelife/mexico/producto-descripcion.blade.php
@@ -9,7 +9,7 @@
     <div class="page-content">
         <!-- Breadcrumb row -->
         <div class="breadcrumb-row" style="margin-top: 80px;">
-            <div class="container">
+            <div class="container-xxl">
                 <ul class="list-inline">
                     <li><a href="{{ route('web_mexico_inicio') }}">Home</a></li>
                     <li><a href="{{ route('web_mexico_productos') }}">Productos</a></li>
@@ -19,19 +19,19 @@
         </div>
         <!-- Breadcrumb row END -->
 
-        <div class="container" style="padding: 60px 0px;">
+        <div class="container-xl" style="padding: 60px 0px;">
             <div class="row">
                 <div class="col-md-6" style="text-align: center;">
-                    <img style="width: 90%;" src="{{ $product->image }}" alt="">
+                    <img style="width: 600px;" src="{{ $product->image }}" alt="">
                 </div>
                 <div class="col-md-6">
                     <!-- Dividers with icon -->
                     <div class="p-a30 bg-white selector-d m-b30">
                         <div class="section-content">
                             <h2 class="text-uppercase">{{ $product->name }}</h2>
-                            <div class="dez-divider divider-2px icon-left" style="background: #00b01a;">
+                            {{-- <div class="dez-divider divider-2px icon-left" style="background: #00b01a;">
                                 <i class="fa fa-thumbs-o-up  text-white" style="background: #00b01a;"></i>
-                            </div>
+                            </div> --}}
                             <div class="m-b10" >
                                 {!! $product->description !!}
                             </div>
@@ -43,13 +43,14 @@
         </div>
 
         <!-- Why Choose Us -->
-        <div class="section-full text-white bg-img-fix p-t70 p-b40 overlay-black-dark choose-us"
+        <div class="section-full text-white bg-img-fix p-t90 p-b90 overlay-black-middle choose-us"
             style="background-image:url({{ asset('themes/zoelife/peru/images/background/fondo.jpg') }});">
-            <div class="container">
+            <div class="container" style="padding: 20px 30px;">
                 <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-md-6" style="padding: 0px 40px;">
                         <div class="section-head  text-center text-white">
-                            <h2 class="h2">Recomendaciones <span style="color: #00b01a;"> de Uso Sugerido</span></h2>
+                            <h2 class="h2 text-uppercase">Recomendaciones <span style="color: #fecc00;"> de Uso
+                                    Sugerido</span></h2>
                             <div class="dez-separator text-primary style-icon border-white">
                                 <img alt="" src="{{ asset('themes/zoelife/peru/images/leaf-green.png') }}" />
                             </div>
@@ -83,34 +84,37 @@
                 <!-- Dividers with icon END -->
                 <br>
 
-                <!-- blog grid -->
-                <div id="masonry" class="dez-blog-grid-3 row">
-                    @if (count($testimonies) > 0)
-                        @foreach ($testimonies as $testimony)
-                            <div class="post card-container col-md-3">
-                                <div class="blog-post blog-grid date-style-2">
-                                    <div class="dez-post-media dez-img-effect zoom-slow">
-                                        {!! $testimony->video !!}
-                                    </div>
-                                    <div class="dez-post-info">
-                                        <div class="dez-post-title ">
-                                            <h3 class="post-title">
-                                                <a href="">
-                                                    {{ $testimony->title }}
-                                                </a>
-                                            </h3>
-                                        </div>
-                                        <div class="dez-post-tags">
-                                            <div class="post-tags">
+                <section>
+                    <div class="container-xxl">
+                        <div class="row">
+                            @if (count($testimonies) > 0)
+                                @foreach ($testimonies as $testimony)
+                                    <div class="col-md-4">
+                                        <div class="blog-post blog-grid date-style-2">
+                                            <div class="dez-post-media dez-img-effect zoom-slow">
+                                                {!! $testimony->video !!}
+                                            </div>
+                                            <div class="dez-post-info">
+                                                <div class="dez-post-title ">
+                                                    <h3 class="post-title">
+                                                        <a href="">
+                                                            {{ $testimony->title }}
+                                                        </a>
+                                                    </h3>
+                                                </div>
+                                                <div class="dez-post-tags">
+                                                    <div class="post-tags">
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
-                        @endforeach
-                    @endif
-                </div>
-                <!-- blog grid END -->
+                                @endforeach
+                            @endif
+                        </div>
+                    </div>
+                </section>
+
             </div>
         </div>
     </div>
