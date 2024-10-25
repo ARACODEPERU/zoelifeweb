7612499df3c0966e73c5721efcf35eb9867c5353
Yisus Web <connexion.jesus@gmail.com>
2024-10-25 16:32:25 -0500

    gty

--- resources/views/zoelife/peru/testimonios.blade.php
+++ resources/views/zoelife/peru/testimonios.blade.php
@@ -35,7 +35,7 @@
             <!-- Breadcrumb row END -->
         </div>
 
-        <div class="content-area" style="padding: 10px 20px;">
+        <div class="container-xxl" style="padding: 10px 20px;">
                 <div class="site-filters clearfix center m-b40">
                     <ul class="filters" data-toggle="buttons">
                         <li data-filter="" class="btn active">
