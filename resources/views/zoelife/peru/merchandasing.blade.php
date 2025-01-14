e12ad205964caa414c2184d9db2ece26120518a9
José Carlos <60234854+Grandejosh@users.noreply.github.com>
2025-01-14 13:54:09 -0500

    s
    s

--- resources/views/zoelife/peru/merchandasing.blade.php
+++ resources/views/zoelife/peru/merchandasing.blade.php
@@ -36,12 +36,12 @@
         <section style="padding: 40px 15px;">
             <div class="container-xxl">
                 <div class="row">
-                    @foreach ($merchandasing as $key => $mer )
+                    @foreach ($merchandasing as $key => $mer)
                         <div class="col-md-3" style="padding: 15px;">
                             <div class="box-producto" style="place-items: center;">
-                                <div  style="place-items: center;">
+                                <div style="place-items: center;">
                                     <div class="carrusel">
-                                        <div class="imagenes" id="imagenes{{ "-".$key + 1}}">
+                                        <div class="imagenes" id="imagenes{{ '-' . $key + 1 }}" style="transform: translateX(0%);">
                                             <div class="imagen">
                                                 <img src="{{ $mer->item->items[0]->content }}" alt="Producto - Imagen 1">
                                             </div>
@@ -52,8 +52,10 @@
                                                 <img src="{{ $mer->item->items[2]->content }}" alt="Producto - Imagen 3">
                                             </div>
                                         </div>
-                                        <button class="flecha izquierda" onclick="anteriorImagen({{ strval($key + 1) }})">&#10094;</button>
-                                        <button class="flecha derecha" onclick="siguienteImagen({{ strval($key + 1) }})">&#10095;</button>
+                                        <button class="flecha izquierda"
+                                            onclick="anteriorImagen('{{ strval($key + 1) }}')">&#10094;</button>
+                                        <button class="flecha derecha"
+                                            onclick="siguienteImagen('{{ strval($key + 1) }}')">&#10095;</button>
                                         <div class="puntos">
                                             <span class="punto" data-index="0"></span>
                                             <span class="punto" data-index="1"></span>
@@ -80,109 +82,134 @@
         </section>
 
         <style>
-
-        .carrusel {
-            position: relative;
-            max-width: 100%; /* Ocupa el 80% de la pantalla */
-            margin: auto;
-            overflow: hidden;
-            border: 2px solid #ddd;
-            border-radius: 10px;
-        }
-        .imagenes {
-            display: flex;
-            transition: transform 0.5s ease-in-out;
-        }
-        .imagen {
-            min-width: 100%; /* Ocupa el 100% del carrusel */
-            box-sizing: border-box;
-            text-align: center;
-        }
-        .imagen img {
-            max-width: 100%; /* Las imágenes ocupan todo el ancho */
-            border-radius: 10px;
-        }
-        .puntos {
-            text-align: center;
-            padding: 10px 0;
-        }
-        .punto {
-            display: inline-block;
-            height: 10px;
-            width: 10px;
-            margin: 0 5px;
-            border-radius: 50%;
-            background-color: #bbb;
-            cursor: pointer;
-        }
-        .activo {
-            background-color: #717171;
-        }
-        .flecha {
-            position: absolute;
-            top: 50%;
-            transform: translateY(-50%);
-            background-color: rgba(255, 255, 255, 0.8);
-            border: none;
-            padding: 10px;
-            cursor: pointer;
-            font-size: 18px;
-            border-radius: 5px;
-            z-index: 1;
-        }
-        .izquierda {
-            left: 10px;
-        }
-        .derecha {
-            right: 10px;
-        }
-
-        /* Estilos responsivos */
-        @media (max-width: 600px) {
-            .flecha {
-                font-size: 16px;
-                padding: 8px;
+            .carrusel {
+                position: relative;
+                max-width: 100%;
+                /* Ocupa el 80% de la pantalla */
+                margin: auto;
+                overflow: hidden;
+                border: 2px solid #ddd;
+                border-radius: 10px;
             }
-        }
+
+            .imagenes {
+                display: flex;
+                transition: transform 0.5s ease-in-out;
+            }
+
+            .imagen {
+                min-width: 100%;
+                /* Ocupa el 100% del carrusel */
+                box-sizing: border-box;
+                text-align: center;
+            }
+
+            .imagen img {
+                max-width: 100%;
+                /* Las imágenes ocupan todo el ancho */
+                border-radius: 10px;
+            }
+
+            .puntos {
+                text-align: center;
+                padding: 10px 0;
+            }
+
+            .punto {
+                display: inline-block;
+                height: 10px;
+                width: 10px;
+                margin: 0 5px;
+                border-radius: 50%;
+                background-color: #bbb;
+                cursor: pointer;
+            }
+
+            .activo {
+                background-color: #717171;
+            }
+
+            .flecha {
+                position: absolute;
+                top: 50%;
+                transform: translateY(-50%);
+                background-color: rgba(255, 255, 255, 0.8);
+                border: none;
+                padding: 10px;
+                cursor: pointer;
+                font-size: 18px;
+                border-radius: 5px;
+                z-index: 1;
+            }
+
+            .izquierda {
+                left: 10px;
+            }
+
+            .derecha {
+                right: 10px;
+            }
+
+            /* Estilos responsivos */
+            @media (max-width: 600px) {
+                .flecha {
+                    font-size: 16px;
+                    padding: 8px;
+                }
+            }
         </style>
 
         <script>
             const imagenes = document.querySelector('.imagenes');
-            const puntos = document.querySelectorAll('.punto');
             const flechaIzquierda = document.querySelector('.izquierda');
             const flechaDerecha = document.querySelector('.derecha');
             let indiceActual = 0;
 
-            function mostrarImagenes(indice, key) { 
-                // let tmp = ".imagenes-"+key;
-                let imagenes2 = document.getElementById("imagenes-"+key.toString())
-                imagenes2.style.transform = `translateX(-${indice * 100}%)`;
-                puntos.forEach((punto, index) => {
-                    punto.classList.toggle('activo', index === indice);
-                });
+            function mostrarImagenes(indice, key) {
+                let imagenes2 = document.getElementById("imagenes-" + key.toString());
+                if (indice == 1) {
+                    imagenes2.style.transform = cambiaTranslate(imagenes2.style.transform, 1);
+                } else {
+                    imagenes2.style.transform = cambiaTranslate(imagenes2.style.transform, 0);
+                }
+
             }
 
             function siguienteImagen(key) {
-                indiceActual = (indiceActual + 1) % puntos.length; // Hay 3 imágenes
-                mostrarImagenes(indiceActual, key);
+                mostrarImagenes(1, key);
             }
 
             function anteriorImagen(key) {
-                indiceActual = (indiceActual - 1 + puntos.length) % puntos.length; // Hay 3 imágenes
-                mostrarImagenes(indiceActual, key);
+                mostrarImagenes(0, key);
             }
 
             flechaDerecha.addEventListener('click', siguienteImagen);
             flechaIzquierda.addEventListener('click', anteriorImagen);
 
-            puntos.forEach(punto => {
-                punto.addEventListener('click', () => {
-                    indiceActual = parseInt(punto.dataset.index);
-                    mostrarImagenes(indiceActual);
-                });
-            });
+            mostrarImagenes(indiceActual);
 
-    mostrarImagenes(indiceActual);
+            function cambiaTranslate(transformValue, val) {
+                // Obtener el valor actual de translateX
+                const match = transformValue.match(/translateX\((-?\d+)%\)/);
+                if (match) {
+                    // Extraer el valor numérico de translateX
+                    const currentValue = parseInt(match[1]);
+                    if(val ==1){
+                        val=-100;
+                        if(currentValue==-200)val=200; // si llego a -200 le suma 200 para dejarlo en 0
+                    }else{
+                        val=100;
+                        if(currentValue==0)val=-200; // si llego a -200 le suma 200 para dejarlo en 0
+                    }
+
+                    // Sumar 100 al valor actual
+                    const newValue = currentValue + val;
+
+                    // Actualizar el valor de translateX con la suma
+
+                    return `translateX(${newValue}%)`;
+                }
+            }
         </script>
 
     </div>
